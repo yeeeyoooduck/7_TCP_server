c737420ac0fdac7a224a1f9006a72cd28ff7916d
Андропов Владимир Викторович <vvandropov@fa.ru>
2024-02-14 18:37:20 +0000

    add fiserver for cv mp4

--- fiserver.py
+++ fiserver.py
@@ -1,23 +1,25 @@
-# Importing the libraries
-import cv2, socket
+# This is server code to send video and audio frames over UDP/TCP
+
+import cv2, imutils, socket
 import numpy as np
 import time
 import base64
-import threading, wave, pyaudio,pickle,struct
+import threading, wave,pickle,struct
 import sys
 import queue
 import os
+# For details visit pyshine.com
 q = queue.Queue(maxsize=10)
 
-filename =  'count.mp4'
-command = "ffmpeg -i {} -ab 160k -ac 2 -ar 44100 -vn {}".format(filename,'temp.wav')
-os.system(command)
+filename =  'sample.mp4'
+
 
 BUFF_SIZE = 65536
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,BUFF_SIZE)
 host_name = socket.gethostname()
-host_ip = '192.168.1.21'#  socket.gethostbyname(host_name)
+host_ip = socket.gethostbyname(host_name)
+# host_ip = '192.168.1.21'#  socket.gethostbyname(host_name)
 print(host_ip)
 port = 9688
 socket_address = (host_ip,port)
@@ -41,6 +43,7 @@ def video_stream_gen():
     while(vid.isOpened()):
         try:
             _,frame = vid.read()
+            frame = imutils.resize(frame,width=WIDTH)
             q.put(frame)
         except:
             os._exit(1)
@@ -90,34 +93,10 @@ def video_stream():
                 break	
                 
 
-def audio_stream():
-    s = socket.socket()
-    s.bind((host_ip, (port-1)))
 
-    s.listen(5)
-    CHUNK = 1024
-    wf = wave.open("temp.wav", 'rb')
-    p = pyaudio.PyAudio()
-    print('server listening at',(host_ip, (port-1)))
-    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                    channels=wf.getnchannels(),
-                    rate=wf.getframerate(),
-                    input=True,
-                    frames_per_buffer=CHUNK)
-
-    client_socket,addr = s.accept()
-
-    while True:
-        if client_socket:
-            while True:
-                data = wf.readframes(CHUNK)
-                a = pickle.dumps(data)
-                message = struct.pack("Q",len(a))+a
-                client_socket.sendall(message)
                 
 
 from concurrent.futures import ThreadPoolExecutor
 with ThreadPoolExecutor(max_workers=3) as executor:
-    executor.submit(audio_stream)
     executor.submit(video_stream_gen)
     executor.submit(video_stream)
